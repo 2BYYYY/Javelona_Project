22d2e44880b8467fe85363118bc4330ffda3dd33
IGNATIUS WARREN BENJAMIN DE LA RAMA JAVELONA <2300689@cc.usls.edu>
2026-01-19 12:26:51 +0800

    fix: layout to layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,6 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>homepage</title>
-    </head>
-    <body>
-        <h1>Welcome to my Portfolio</h1>
-    </body>
-</html> -->
-
-@extends('layout.content')
+@extends('layouts.content')
 
 @section('title', 'home')
 
-@section('content')
+@section('layouts.content')
 
